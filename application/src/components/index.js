6162313c9dc14d0863eb668a681facebac6f856e
Alan Bedoya <alanjbedoya@gmail.com>
2020-04-08 00:15:39 -0700

    feat (route guarding): added functionality to redirect the user to /login to prevent viewing of unprotected routes.

--- application/src/components/index.js
+++ application/src/components/index.js
@@ -4,5 +4,6 @@ import Nav from './nav/nav';
 import OrderForm from './order-form/orderForm';
 import Template from './common/template';
 import ViewOrders from './view-orders/viewOrders';
+import PrivateRoute from '../router/privateRoute';
 
-export { Login, OrderForm, Main, Nav, Template, ViewOrders };
+export { Login, PrivateRoute, OrderForm, Main, Nav, Template, ViewOrders };
